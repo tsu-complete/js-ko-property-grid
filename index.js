078a4e91983ccb1a7844b845e8c7e6d4757b53d1
tsukumo <tsu.complete@gmail.com>
2015-08-06 21:05:53 -0400

    Add model skeletons and setup ko component
    Also exporting component property model for user
    extension purposes

--- index.js
+++ index.js
@@ -13,6 +13,45 @@
 })(function ( ko ) {
     "use strict";
 
+    var PropertyModel, PropertyGridViewModel;
+
+    PropertyModel = (function ( ) {
+        var proto;
+
+        function PropertyModel ( options ) {
+            // TODO: implement
+            this.options = options;
+
+        }
+
+        proto = PropertyModel.prototype;
+
+        return PropertyModel;
+    })();
+
+    PropertyGridViewModel = (function (  ) {
+        var proto;
+
+        function PropertyGridViewModel ( params ) {
+            // TODO: implement
+            this.params = params;
+
+        }
+
+        proto = PropertyGridViewModel.prototype;
+
+        return PropertyGridViewModel;
+    })();
+
+    ko.components.register("property-grid", {
+        viewModel: PropertyGridViewModel
+    ,   synchronous: true
+    ,   template: "<!-- TODO -->"
+    });
+
+    ko.property_grid = {
+        PropertyModel: PropertyModel
+    };
 
     return ko;
 });
